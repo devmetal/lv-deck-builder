a0e8d4a77e63528fc206d87f833445b64e8e3c43
devmetal <devmetal91@gmail.com>
2025-02-06 14:45:04 +0100

    install radix instead of headlessui

--- resources/js/Pages/Welcome.tsx
+++ resources/js/Pages/Welcome.tsx
@@ -1,15 +1,11 @@
 import { Head } from "@inertiajs/react";
 import styled from "styled-components";
-
-const Title = styled.h1`
-  font-size: 1.5em;
-  text-align: center;
-  color: #BF4F74;
-`;
+import { Avatar } from "radix-ui";
 
 const Wrapper = styled.section`
   padding: 4em;
   background: papayawhip;
+  display: flex;
 `;
 
 export default function Welcome() {
@@ -17,7 +13,16 @@ export default function Welcome() {
 		<>
 			<Head title="Welcome" />
 			<Wrapper>
-				<Title>Welcome</Title>
+				<Avatar.Root className="AvatarRoot">
+					<Avatar.Image
+						className="AvatarImage"
+						src="https://images.unsplash.com/photo-1492633423870-43d1cd2775eb?&w=128&h=128&dpr=2&q=80"
+						alt="Colm Tuite"
+					/>
+					<Avatar.Fallback className="AvatarFallback" delayMs={600}>
+						CT
+					</Avatar.Fallback>
+				</Avatar.Root>
 			</Wrapper>
 		</>
 	);
