66d88d2875a9fd17ba3993b02d28505e521d3516
devmetal <devmetal91@gmail.com>
2025-02-06 14:29:56 +0100

    chore: add styled-components

--- resources/js/Pages/Welcome.tsx
+++ resources/js/Pages/Welcome.tsx
@@ -1,10 +1,24 @@
 import { Head } from "@inertiajs/react";
+import styled from "styled-components";
+
+const Title = styled.h1`
+  font-size: 1.5em;
+  text-align: center;
+  color: #BF4F74;
+`;
+
+const Wrapper = styled.section`
+  padding: 4em;
+  background: papayawhip;
+`;
 
 export default function Welcome() {
 	return (
 		<>
 			<Head title="Welcome" />
-			<h1>Welcome</h1>
+			<Wrapper>
+				<Title>Welcome</Title>
+			</Wrapper>
 		</>
 	);
 }
