0ed6cdf28d3355eab11a08334fb04426e4013b3e
devmetal <devmetal91@gmail.com>
2025-02-21 18:02:26 +0100

    feat(import): add more test cases

--- tests/Feature/ImportCardsTest.php
+++ tests/Feature/ImportCardsTest.php
@@ -45,4 +45,54 @@ class ImportCardsTest extends TestCase
 
         Queue::assertPushed(ProcessImportedCard::class, 3);
     }
+
+    public function test_validation_error_when_file_missing()
+    {
+        Queue::fake();
+
+        $user = User::factory()->create();
+
+        $response = $this
+            ->actingAs($user)
+            ->post('/import/cards/upload');
+
+        $response->assertInvalid(['file']);
+    }
+
+    public function test_validation_error_when_file_is_not_csv()
+    {
+        Queue::fake();
+
+        $user = User::factory()->create();
+
+        $file = UploadedFile::fake()->image('wrong.png');
+
+        $response = $this
+            ->actingAs($user)
+            ->post('/import/cards/upload', [
+                'file' => $file,
+            ]);
+
+        $response->assertInvalid(['file']);
+    }
+
+    public function test_validation_error_when_csv_does_not_contain_lines_enough()
+    {
+        Queue::fake();
+
+        $user = User::factory()->create();
+
+        $csv = ['scry_id'];
+
+        $file = UploadedFile::fake()
+            ->createWithContent('ids.csv', implode(PHP_EOL, $csv));
+
+        $response = $this
+            ->actingAs($user)
+            ->post('/import/cards/upload', [
+                'file' => $file,
+            ]);
+
+        $response->assertInvalid(['file']);
+    }
 }
