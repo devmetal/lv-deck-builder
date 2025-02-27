47050296c007245781bfdf1a85d3acfeeecf35cd
devmetal <devmetal91@gmail.com>
2025-02-27 17:44:35 +0100

    feat: add faces to the cards

--- resources/js/Components/Card/Card.tsx
+++ resources/js/Components/Card/Card.tsx
@@ -1,6 +1,30 @@
+import { motion } from 'motion/react';
+import { PointerEventHandler, useState } from 'react';
+
 export default function Card({ card }: { card: App.Domain.Dto.FeCard }) {
+  const [facesVisible, setFacesVisible] = useState(false);
+  const [facesDirection, setFacesDirection] = useState<'left' | 'right'>(
+    'right',
+  );
+
+  const { faces } = card;
+
   // get the main image, if its not avaiable switch to first face
-  const image = card.image?.png ?? card.faces?.[0]?.image?.png ?? null;
+  const image = card.image?.png ?? faces?.[0]?.image?.png ?? null;
+
+  const showFaces: PointerEventHandler = (e) => {
+    const { currentTarget } = e;
+    if (currentTarget.getBoundingClientRect().x > window.innerWidth / 2) {
+      setFacesDirection('left');
+    } else {
+      setFacesDirection('right');
+    }
+    setFacesVisible(true);
+  };
+
+  const hideFaces = () => {
+    setFacesVisible(false);
+  };
 
   if (!image) {
     return null;
@@ -8,7 +32,33 @@ export default function Card({ card }: { card: App.Domain.Dto.FeCard }) {
 
   return (
     <div className="flex flex-col items-center gap-2">
-      <img src={image} className="w-full drop-shadow-lg" />
+      <motion.div
+        whileHover={{ scale: 1.2, rotate: 2, zIndex: 1 }}
+        whileTap={{ scale: 1.2, rotate: 2, zIndex: 1 }}
+        className={`flex ${facesDirection === 'right' ? 'flex-row' : 'flex-row-reverse'}`}
+        onPointerLeave={hideFaces}
+        onPointerEnter={showFaces}
+      >
+        <img src={image} className="w-full" />
+        {facesVisible && (
+          <>
+            {faces
+              ?.filter((face) => face.image?.png)
+              ?.map((face) => face.image?.png)
+              ?.filter((png) => png !== image)
+              ?.map((png) => (
+                <motion.img
+                  initial={{ opacity: 0 }}
+                  animate={{ opacity: 1 }}
+                  className="w-full z-10"
+                  key={png}
+                  src={png}
+                />
+              ))}
+          </>
+        )}
+      </motion.div>
+
       <div className="badge">{card.price} USD</div>
     </div>
   );
--- resources/js/Pages/Cards/Partials/CardList.tsx
+++ resources/js/Pages/Cards/Partials/CardList.tsx
@@ -2,7 +2,7 @@ import Card from '@/Components/Card';
 
 function CardList(props: { cards: App.Domain.Dto.FeCard[] }) {
   return (
-    <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-4 lg:grid-cols-5 xl:grid-cols-6 gap-4 m-8 mx-16 md:mx-8 ">
+    <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-4 lg:grid-cols-5 xl:grid-cols-7 gap-4 m-8 mx-16 md:mx-8 xl:mx-24">
       {props.cards?.map((card) => <Card card={card} key={card.id} />)}
     </div>
   );
