040308350a6824a5bdd283c5d85888131d30432e
devmetal <devmetal91@gmail.com>
2025-07-02 14:32:19 +0200

    chore: update and dump db schema

--- database/schema/pgsql-schema.sql
+++ database/schema/pgsql-schema.sql
@@ -2,8 +2,8 @@
 -- PostgreSQL database dump
 --
 
--- Dumped from database version 14.15 (Ubuntu 14.15-0ubuntu0.22.04.1)
--- Dumped by pg_dump version 14.15 (Ubuntu 14.15-0ubuntu0.22.04.1)
+-- Dumped from database version 14.18 (Ubuntu 14.18-0ubuntu0.22.04.1)
+-- Dumped by pg_dump version 14.18 (Ubuntu 14.18-0ubuntu0.22.04.1)
 
 SET statement_timeout = 0;
 SET lock_timeout = 0;
@@ -43,10 +43,20 @@ CREATE TABLE public.cache_locks (
 
 
 --
--- Name: card_decks; Type: TABLE; Schema: public; Owner: -
+-- Name: card_category; Type: TABLE; Schema: public; Owner: -
 --
 
-CREATE TABLE public.card_decks (
+CREATE TABLE public.card_category (
+    card_id bigint NOT NULL,
+    category_id bigint NOT NULL
+);
+
+
+--
+-- Name: card_deck; Type: TABLE; Schema: public; Owner: -
+--
+
+CREATE TABLE public.card_deck (
     card_id bigint NOT NULL,
     deck_id bigint NOT NULL,
     "column" smallint DEFAULT '0'::smallint NOT NULL,
@@ -66,12 +76,13 @@ CREATE TABLE public.cards (
     scry_id character varying(255) NOT NULL,
     colors json,
     keywords json,
-    oracle_text character varying(255),
+    oracle_text text,
     cmc smallint NOT NULL,
     type_line character varying(255) NOT NULL,
     user_id bigint NOT NULL,
     set_id bigint NOT NULL,
-    image_id bigint NOT NULL
+    image_id bigint,
+    rarity character varying(255) NOT NULL
 );
 
 
@@ -92,6 +103,38 @@ CREATE SEQUENCE public.cards_id_seq
 --
 
 ALTER SEQUENCE public.cards_id_seq OWNED BY public.cards.id;
+
+
+--
+-- Name: categories; Type: TABLE; Schema: public; Owner: -
+--
+
+CREATE TABLE public.categories (
+    id bigint NOT NULL,
+    created_at timestamp(0) without time zone,
+    updated_at timestamp(0) without time zone,
+    name character varying(255) NOT NULL,
+    view_id bigint NOT NULL
+);
+
+
+--
+-- Name: categories_id_seq; Type: SEQUENCE; Schema: public; Owner: -
+--
+
+CREATE SEQUENCE public.categories_id_seq
+    START WITH 1
+    INCREMENT BY 1
+    NO MINVALUE
+    NO MAXVALUE
+    CACHE 1;
+
+
+--
+-- Name: categories_id_seq; Type: SEQUENCE OWNED BY; Schema: public; Owner: -
+--
+
+ALTER SEQUENCE public.categories_id_seq OWNED BY public.categories.id;
 
 
 --
@@ -139,10 +182,10 @@ CREATE TABLE public.faces (
     updated_at timestamp(0) without time zone,
     name character varying(255) NOT NULL,
     colors json,
-    oracle_text character varying(255),
+    oracle_text text,
     cmc smallint,
     type_line character varying(255),
-    image_id bigint NOT NULL,
+    image_id bigint,
     card_id bigint NOT NULL
 );
 
@@ -363,6 +406,42 @@ CREATE TABLE public.password_reset_tokens (
 
 
 --
+-- Name: prices; Type: TABLE; Schema: public; Owner: -
+--
+
+CREATE TABLE public.prices (
+    id bigint NOT NULL,
+    created_at timestamp(0) without time zone,
+    updated_at timestamp(0) without time zone,
+    mtgjson_uuid character varying(255) NOT NULL,
+    provider character varying(255) NOT NULL,
+    currency character varying(255) NOT NULL,
+    price double precision NOT NULL,
+    scry_id character varying(255) NOT NULL,
+    source_date date NOT NULL
+);
+
+
+--
+-- Name: prices_id_seq; Type: SEQUENCE; Schema: public; Owner: -
+--
+
+CREATE SEQUENCE public.prices_id_seq
+    START WITH 1
+    INCREMENT BY 1
+    NO MINVALUE
+    NO MAXVALUE
+    CACHE 1;
+
+
+--
+-- Name: prices_id_seq; Type: SEQUENCE OWNED BY; Schema: public; Owner: -
+--
+
+ALTER SEQUENCE public.prices_id_seq OWNED BY public.prices.id;
+
+
+--
 -- Name: sessions; Type: TABLE; Schema: public; Owner: -
 --
 
@@ -445,10 +524,50 @@ ALTER SEQUENCE public.users_id_seq OWNED BY public.users.id;
 
 
 --
+-- Name: views; Type: TABLE; Schema: public; Owner: -
+--
+
+CREATE TABLE public.views (
+    id bigint NOT NULL,
+    created_at timestamp(0) without time zone,
+    updated_at timestamp(0) without time zone,
+    name character varying(255) NOT NULL,
+    description character varying(255),
+    deck_id bigint NOT NULL
+);
+
+
+--
+-- Name: views_id_seq; Type: SEQUENCE; Schema: public; Owner: -
+--
+
+CREATE SEQUENCE public.views_id_seq
+    START WITH 1
+    INCREMENT BY 1
+    NO MINVALUE
+    NO MAXVALUE
+    CACHE 1;
+
+
+--
+-- Name: views_id_seq; Type: SEQUENCE OWNED BY; Schema: public; Owner: -
+--
+
+ALTER SEQUENCE public.views_id_seq OWNED BY public.views.id;
+
+
+--
 -- Name: cards id; Type: DEFAULT; Schema: public; Owner: -
 --
 
 ALTER TABLE ONLY public.cards ALTER COLUMN id SET DEFAULT nextval('public.cards_id_seq'::regclass);
+
+
+--
+-- Name: categories id; Type: DEFAULT; Schema: public; Owner: -
+--
+
+ALTER TABLE ONLY public.categories ALTER COLUMN id SET DEFAULT nextval('public.categories_id_seq'::regclass);
 
 
 --
@@ -501,6 +620,13 @@ ALTER TABLE ONLY public.migrations ALTER COLUMN id SET DEFAULT nextval('public.m
 
 
 --
+-- Name: prices id; Type: DEFAULT; Schema: public; Owner: -
+--
+
+ALTER TABLE ONLY public.prices ALTER COLUMN id SET DEFAULT nextval('public.prices_id_seq'::regclass);
+
+
+--
 -- Name: sets id; Type: DEFAULT; Schema: public; Owner: -
 --
 
@@ -512,6 +638,13 @@ ALTER TABLE ONLY public.sets ALTER COLUMN id SET DEFAULT nextval('public.sets_id
 --
 
 ALTER TABLE ONLY public.users ALTER COLUMN id SET DEFAULT nextval('public.users_id_seq'::regclass);
+
+
+--
+-- Name: views id; Type: DEFAULT; Schema: public; Owner: -
+--
+
+ALTER TABLE ONLY public.views ALTER COLUMN id SET DEFAULT nextval('public.views_id_seq'::regclass);
 
 
 --
@@ -531,10 +664,18 @@ ALTER TABLE ONLY public.cache
 
 
 --
--- Name: card_decks card_decks_card_id_deck_id_unique; Type: CONSTRAINT; Schema: public; Owner: -
+-- Name: card_category card_category_pkey; Type: CONSTRAINT; Schema: public; Owner: -
 --
 
-ALTER TABLE ONLY public.card_decks
+ALTER TABLE ONLY public.card_category
+    ADD CONSTRAINT card_category_pkey PRIMARY KEY (card_id, category_id);
+
+
+--
+-- Name: card_deck card_decks_card_id_deck_id_unique; Type: CONSTRAINT; Schema: public; Owner: -
+--
+
+ALTER TABLE ONLY public.card_deck
     ADD CONSTRAINT card_decks_card_id_deck_id_unique UNIQUE (card_id, deck_id);
 
 
@@ -544,6 +685,22 @@ ALTER TABLE ONLY public.card_decks
 
 ALTER TABLE ONLY public.cards
     ADD CONSTRAINT cards_pkey PRIMARY KEY (id);
+
+
+--
+-- Name: categories categories_name_unique; Type: CONSTRAINT; Schema: public; Owner: -
+--
+
+ALTER TABLE ONLY public.categories
+    ADD CONSTRAINT categories_name_unique UNIQUE (name);
+
+
+--
+-- Name: categories categories_pkey; Type: CONSTRAINT; Schema: public; Owner: -
+--
+
+ALTER TABLE ONLY public.categories
+    ADD CONSTRAINT categories_pkey PRIMARY KEY (id);
 
 
 --
@@ -627,6 +784,14 @@ ALTER TABLE ONLY public.password_reset_tokens
 
 
 --
+-- Name: prices prices_pkey; Type: CONSTRAINT; Schema: public; Owner: -
+--
+
+ALTER TABLE ONLY public.prices
+    ADD CONSTRAINT prices_pkey PRIMARY KEY (id);
+
+
+--
 -- Name: sessions sessions_pkey; Type: CONSTRAINT; Schema: public; Owner: -
 --
 
@@ -659,6 +824,43 @@ ALTER TABLE ONLY public.users
 
 
 --
+-- Name: views views_name_unique; Type: CONSTRAINT; Schema: public; Owner: -
+--
+
+ALTER TABLE ONLY public.views
+    ADD CONSTRAINT views_name_unique UNIQUE (name);
+
+
+--
+-- Name: views views_pkey; Type: CONSTRAINT; Schema: public; Owner: -
+--
+
+ALTER TABLE ONLY public.views
+    ADD CONSTRAINT views_pkey PRIMARY KEY (id);
+
+
+--
+-- Name: cards_name_fulltext; Type: INDEX; Schema: public; Owner: -
+--
+
+CREATE INDEX cards_name_fulltext ON public.cards USING gin (to_tsvector('english'::regconfig, (name)::text));
+
+
+--
+-- Name: cards_oracle_text_fulltext; Type: INDEX; Schema: public; Owner: -
+--
+
+CREATE INDEX cards_oracle_text_fulltext ON public.cards USING gin (to_tsvector('english'::regconfig, oracle_text));
+
+
+--
+-- Name: cards_type_line_fulltext; Type: INDEX; Schema: public; Owner: -
+--
+
+CREATE INDEX cards_type_line_fulltext ON public.cards USING gin (to_tsvector('english'::regconfig, (type_line)::text));
+
+
+--
 -- Name: jobs_queue_index; Type: INDEX; Schema: public; Owner: -
 --
 
@@ -680,18 +882,34 @@ CREATE INDEX sessions_user_id_index ON public.sessions USING btree (user_id);
 
 
 --
--- Name: card_decks card_decks_card_id_foreign; Type: FK CONSTRAINT; Schema: public; Owner: -
+-- Name: card_category card_category_card_id_foreign; Type: FK CONSTRAINT; Schema: public; Owner: -
 --
 
-ALTER TABLE ONLY public.card_decks
+ALTER TABLE ONLY public.card_category
+    ADD CONSTRAINT card_category_card_id_foreign FOREIGN KEY (card_id) REFERENCES public.cards(id) ON UPDATE CASCADE ON DELETE CASCADE;
+
+
+--
+-- Name: card_category card_category_category_id_foreign; Type: FK CONSTRAINT; Schema: public; Owner: -
+--
+
+ALTER TABLE ONLY public.card_category
+    ADD CONSTRAINT card_category_category_id_foreign FOREIGN KEY (category_id) REFERENCES public.categories(id) ON UPDATE CASCADE ON DELETE CASCADE;
+
+
+--
+-- Name: card_deck card_decks_card_id_foreign; Type: FK CONSTRAINT; Schema: public; Owner: -
+--
+
+ALTER TABLE ONLY public.card_deck
     ADD CONSTRAINT card_decks_card_id_foreign FOREIGN KEY (card_id) REFERENCES public.cards(id) ON UPDATE CASCADE ON DELETE CASCADE;
 
 
 --
--- Name: card_decks card_decks_deck_id_foreign; Type: FK CONSTRAINT; Schema: public; Owner: -
+-- Name: card_deck card_decks_deck_id_foreign; Type: FK CONSTRAINT; Schema: public; Owner: -
 --
 
-ALTER TABLE ONLY public.card_decks
+ALTER TABLE ONLY public.card_deck
     ADD CONSTRAINT card_decks_deck_id_foreign FOREIGN KEY (deck_id) REFERENCES public.decks(id) ON DELETE CASCADE;
 
 
@@ -717,6 +935,14 @@ ALTER TABLE ONLY public.cards
 
 ALTER TABLE ONLY public.cards
     ADD CONSTRAINT cards_user_id_foreign FOREIGN KEY (user_id) REFERENCES public.users(id) ON UPDATE CASCADE ON DELETE CASCADE;
+
+
+--
+-- Name: categories categories_view_id_foreign; Type: FK CONSTRAINT; Schema: public; Owner: -
+--
+
+ALTER TABLE ONLY public.categories
+    ADD CONSTRAINT categories_view_id_foreign FOREIGN KEY (view_id) REFERENCES public.views(id) ON UPDATE CASCADE ON DELETE CASCADE;
 
 
 --
@@ -760,6 +986,14 @@ ALTER TABLE ONLY public.sets
 
 
 --
+-- Name: views views_deck_id_foreign; Type: FK CONSTRAINT; Schema: public; Owner: -
+--
+
+ALTER TABLE ONLY public.views
+    ADD CONSTRAINT views_deck_id_foreign FOREIGN KEY (deck_id) REFERENCES public.decks(id) ON UPDATE CASCADE ON DELETE CASCADE;
+
+
+--
 -- PostgreSQL database dump complete
 --
 
@@ -767,8 +1001,8 @@ ALTER TABLE ONLY public.sets
 -- PostgreSQL database dump
 --
 
--- Dumped from database version 14.15 (Ubuntu 14.15-0ubuntu0.22.04.1)
--- Dumped by pg_dump version 14.15 (Ubuntu 14.15-0ubuntu0.22.04.1)
+-- Dumped from database version 14.18 (Ubuntu 14.18-0ubuntu0.22.04.1)
+-- Dumped by pg_dump version 14.18 (Ubuntu 14.18-0ubuntu0.22.04.1)
 
 SET statement_timeout = 0;
 SET lock_timeout = 0;
@@ -803,6 +1037,21 @@ COPY public.migrations (id, migration, batch) FROM stdin;
 15	2025_02_21_093226_remove_scry_data_from_faces_table	4
 16	2025_02_21_113127_drop_scry_data_from_faces_table	4
 17	2025_02_21_113150_drop_scry_data_from_cards_table	4
+18	2025_02_21_192141_change_text_fields_in_cards_table	5
+19	2025_02_21_192416_change_text_fields_in_faces_table	5
+20	2025_02_21_192909_change_image_id_to_nullable_in_cards_table	6
+21	2025_02_21_193046_change_image_id_to_nullable_in_faces_table	7
+22	2025_02_22_201435_create_prices_table	8
+23	2025_02_23_140400_add_provider_column_to_prices_table	9
+24	2025_02_23_141847_remove_card_id_column_from_prices_table	10
+25	2025_02_23_142008_add_scry_id_to_prices_table	10
+26	2025_02_23_165343_drop_prices_table	11
+27	2025_02_24_095526_create_prices_table	12
+28	2025_02_24_103027_add_source_date_to_prices_table	13
+29	2025_02_26_194034_add-full-text-indexes-to-cards-table	14
+30	2025_03_07_065941_add_rarity_to_cards_table	15
+31	2025_04_11_075749_rename_card_deck_table	16
+32	2025_07_02_121610_update-overall-cards-decks-table-structure	17
 \.
 
 
@@ -810,7 +1059,7 @@ COPY public.migrations (id, migration, batch) FROM stdin;
 -- Name: migrations_id_seq; Type: SEQUENCE SET; Schema: public; Owner: -
 --
 
-SELECT pg_catalog.setval('public.migrations_id_seq', 17, true);
+SELECT pg_catalog.setval('public.migrations_id_seq', 32, true);
 
 
 --
